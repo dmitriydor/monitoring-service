e00969963f0c0e1984f53011b48af4851d9677ca
Doronin <dmitriy16doronin@gmail.com>
2020-03-25 11:25:39 +0700

    Interim commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+class Item{
+  userName:string;
+  statisticsDate:string;
+  appVersion:string;
+  operationSystem:string;
+  
+  constructor(userName:string, statisticsDate:string, appVersion:string, operationSystem:string) {
+    this.userName = userName;
+    this.statisticsDate = statisticsDate;
+    this.appVersion = appVersion;
+    this.operationSystem = operationSystem;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monitoring-service';
+  items: Item[] = 
+    [
+      { userName: "Dmitriy Doronin", statisticsDate: new Date().toLocaleString(), appVersion: "2.2.4", operationSystem: "android" },
+      { userName: "Dmitriy Doronin", statisticsDate: new Date().toLocaleString(), appVersion: "2.2.4", operationSystem: "android" },
+      { userName: "Dmitriy Doronin", statisticsDate: new Date().toLocaleString(), appVersion: "2.2.4", operationSystem: "android" },
+      { userName: "Dmitriy Doronin", statisticsDate: new Date().toLocaleString(), appVersion: "2.2.4", operationSystem: "android" },
+      { userName: "Dmitriy Doronin", statisticsDate: new Date().toLocaleString(), appVersion: "2.2.4", operationSystem: "android" },
+    ];
 }
