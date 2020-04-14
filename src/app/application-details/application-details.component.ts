a619acc0455cd00851d97b54b067c700f6b54218
Dmitriy <dmitriy16doronin@gmail.com>
2020-04-14 12:43:57 +0700

    Fixed display of elements

--- src/app/application-details/application-details.component.ts
+++ src/app/application-details/application-details.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ApplicationService} from "../services/application.service";
 import {ActivatedRoute} from "@angular/router";
 import {ApplicationViewModel} from "../models/application-view-model.model";
-import {distinctUntilChanged,} from "rxjs/operators";
-
+import {distinctUntilChanged, flatMap,} from "rxjs/operators";
 
 @Component({
   selector: 'app-application-details',
@@ -13,13 +12,13 @@ import {distinctUntilChanged,} from "rxjs/operators";
 export class ApplicationDetailsComponent implements OnInit {
   application: ApplicationViewModel;
   displayedColumns: string[] = ['date', 'eventName', 'description'];
-  id: string;
 
   constructor(private applicationService: ApplicationService, private route: ActivatedRoute) {
     this.route.params
-      .pipe(distinctUntilChanged())
-      .subscribe( (params) => this.applicationService.loadById(params['id'])
-        .subscribe( app => this.application = app));
+      .pipe(
+        distinctUntilChanged(),
+        flatMap(params => this.applicationService.loadById(params['id'])))
+      .subscribe(app => this.application = app);
   }
 
   ngOnInit(): void {}
--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Application } from 'src/app/models/application.model';
 import { Observable } from 'rxjs';
-import {flatMap, map, mergeMap, tap} from 'rxjs/operators'
+import {map} from 'rxjs/operators'
 import { ApplicationViewModel } from 'src/app/models/application-view-model.model';
 @Injectable()
 export class ApplicationService {
@@ -11,17 +11,17 @@ export class ApplicationService {
 
   loadAll(): Observable<Application[]> {
     return this.http.get<Application[]>('https://localhost:5001/api/application')
-    .pipe(map((data: ApplicationViewModel[]) => data.map(item => {
-      return {
-        id: item.id,
-        date: new Date(item.date).toLocaleString(),
-        userName: item.userName,
-        appVersion: item.appVersion,
-        operationSystem: item.operationSystem
-      };
+      .pipe(map((data: ApplicationViewModel[]) => data.map(item => {
+        return {
+          id: item.id,
+          date: new Date(item.date).toLocaleString(),
+          userName: item.userName,
+          appVersion: item.appVersion,
+          operationSystem: item.operationSystem
+        };
       })));
   }
   loadById(appId:string):Observable<ApplicationViewModel>{
-    return this.http.get<ApplicationViewModel>(`https://localhost:5001/api/application/${appId}`).pipe(tap( x => console.log(x)));
+    return this.http.get<ApplicationViewModel>(`https://localhost:5001/api/application/${appId}`);
   }
 }
